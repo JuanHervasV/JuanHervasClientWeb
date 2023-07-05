c9066f06f1a1776c42dc4558a0bed061d3477af1
juanhervasv <juan.19.6.96@gmail.com>
2023-07-04 19:26:23 -0500

    quitando el negrita, arreglando el html

--- src/components/Web/PoliticaPrivacidad/PoliticaPrivacidad.js
+++ src/components/Web/PoliticaPrivacidad/PoliticaPrivacidad.js
@@ -5,19 +5,19 @@ export default function PoliticaPrivacidad(){
         <div>
         <p align="justify"><b>Política de Privacidad</b></p>
 <p><n>Adivina el Número</n></p>
-<p><n>Última actualización: 04 Julio 2023</p>
+<p>Última actualización: 04 Julio 2023</p>
 <p align="justify"><n>Esta Política de Privacidad describe cómo se manejan los datos en la aplicación móvil &quot;Adivina el Número&quot; (&quot;nosotros&quot;, &quot;nuestro&quot;). Nos comprometemos a proteger la privacidad de nuestros usuarios y a garantizar la confidencialidad de la información proporcionada. Esta política se aplica al uso de la aplicación y describe cómo se gestionan los datos recopilados. </p>
-<p><n>Recopilación de Datos:</p>
+<p>Recopilación de Datos:</p>
 <p align="justify"><n>Adivina el Número no recopila ningún tipo de datos personales identificables de los usuarios. No solicitamos ni requerimos información como nombres, direcciones de correo electrónico, números de teléfono u otra información personal para utilizar nuestra aplicación. No realizamos seguimiento del comportamiento del usuario ni recopilamos datos para fines publicitarios. </p>
-<p><n>Uso de los Datos:</p>
+<p>Uso de los Datos:</p>
 <p align="justify"><n>Dado que no recopilamos datos personales identificables, no utilizamos ninguna información para ningún propósito. No compartimos, vendemos ni alquilamos información con terceros.</p>
-<p><n>Acceso a los Datos:</p>
+<p>Acceso a los Datos:</p>
 <p align="justify"><n>Adivina el Número no accede a ningún dato personal del usuario ni recopila información en relación con el uso de la aplicación.</p>
-<p><n>Compartir los Datos:</p>
+<p>Compartir los Datos:</p>
 <p align="justify"><n>Dado que no recopilamos datos personales identificables, no compartimos ningún dato de identificación personal con terceros.</p>
-<p><n>Política de Privacidad en el App:</p>
+<p>Política de Privacidad en el App:</p>
 <p align="justify"><n>La presente Política de Privacidad se muestra en la sección Contacto del Desarrollador de la aplicación en el Play Store para que los usuarios puedan acceder y revisarla fácilmente.</p>
-<p><n>Actualizaciones de la Política de Privacidad:</p>
+<p>Actualizaciones de la Política de Privacidad:</p>
 <p align="justify"><n>Nos reservamos el derecho de actualizar o modificar esta Política de Privacidad en cualquier momento. Cualquier cambio realizado se publicará en la sección Contacto del Desarrollador de la aplicación. Los usuarios serán notificados acerca de estos cambios dentro de la aplicación. Se recomienda a los usuarios revisar periódicamente esta Política de Privacidad para mantenerse informados sobre cómo protegemos su información.</p>
 <p align="justify"><n>Si tienes alguna pregunta o inquietud sobre esta Política de Privacidad, puedes comunicarte con nosotros a través de la información de contacto proporcionada en la aplicación.</p>
 <p align="justify"><n>Recuerda que esta política de privacidad se aplica específicamente a la aplicación &quot;Adivina el Número&quot; y su manejo de datos. Como no recopilamos ningún tipo de dato personal, cumplimos con el compromiso de mantener la privacidad de nuestros usuarios y la confidencialidad de su información.</p>
